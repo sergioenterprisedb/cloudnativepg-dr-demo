b9e1f7afa482fa8b201713a6b8c853d6c25f1cd7
Sergio Romera <sergio.romera@enterprisedb.com>
2025-08-29 15:30:12 +0200

    Update

--- install_secrets.sh
+++ install_secrets.sh
@@ -6,20 +6,21 @@
 
 . ./config_cloudnativepg.sh
 
+kubectl delete secret minio-creds
+kubectl delete secret aws-creds
+kubectl delete secret app-secret
+
 if [ "${OBJECT_STORAGE}" == "AWS" ]; then
   kubectl create secret generic aws-creds \
     --from-literal=ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
     --from-literal=ACCESS_SECRET_KEY="$AWS_SECRET_ACCESS_KEY" \
     --from-literal=ACCESS_SESSION_TOKEN="$AWS_SESSION_TOKEN"
+elif [ "${OBJECT_STORAGE}" == "MINIO" ]; then
+  kubectl create secret generic minio-creds \
+    --from-literal=MINIO_ACCESS_KEY=admin \
+    --from-literal=MINIO_SECRET_KEY=password
 fi
 
-#kubectl apply -f app-secret.yaml
 kubectl create secret generic app-secret --from-literal=username=app --from-literal=password=app
 
-#kubectl apply -f superuser-secret.yaml
-kubectl create secret generic superuser-secret --from-literal=username=postgres --from-literal=password=admin
-
-kubectl create secret generic minio-creds \
-  --from-literal=MINIO_ACCESS_KEY=admin \
-  --from-literal=MINIO_SECRET_KEY=password
 
